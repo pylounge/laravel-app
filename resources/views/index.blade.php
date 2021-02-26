0aec7be76f356e9706dc679a149610da4e715809
Maxim <email@mail.ru>
2021-02-26 14:50:35 +0300

    second lesson

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main', ['title' => 'Заголовок'])
+
+@section('content')
+    Привет, {{ $name }}!
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ $title ?? 'Laravel' }}</title>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <header>
+        <div class="title">Web-программирование</div>
+        <nav>
+            <ul>
+                   <li>
+                       <a href="#">1</a>
+                   </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <aside>Боковая панель</aside>
+        <section>
+    <body>
+        @yield('content')
+    </section>
+</main>
+<footer>
+    &copy; ЕГУ
+</footer>
+        <script src="{{ asset('js/manifest.js') }}"></script>
+        <script src="{{ asset('js/vendor.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
